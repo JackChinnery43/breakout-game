c02056f85248733f514ddb741c0551b74eb8271b
JackChinnery43 <jack-chinnery@hotmail.com>
2021-03-09 15:02:03 +0000

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,35 +1,17 @@
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.KeyCode;
 
-/**
-*   The breakout controller receives KeyPress events from the GUI (via
-*   the KeyEventHandler). It maps the keys onto methods in the model and
-*   calls them appropriately
-*/
-
 public class Controller
 {
     public Model model;
     public View  view;
     
-    // we don't really need a constructor method, but include one to print a 
-    // debugging message if required
+   
     public Controller()
     {
         Debug.trace("Controller::<constructor>");
     }
 
-    /**
-    *   This is how the View talks to the Controller
-    *   AND how the Controller talks to the Model
-    *   This method is called by the View to respond to key presses in the GUI
-    *   The controller's job is to decide what to do. In this case it converts
-    *   the keypresses into commands which are run in the model.
-    *   I edited the template code provided to us to use several if statements
-    *   instead of the switch statement that was provided to us.
-    *   This was because I understand if statements more than I understand
-    *   switch statements, and so I was more confident making changes to the code.
-    */
     
     public void userKeyInteraction(KeyEvent event)
     {
